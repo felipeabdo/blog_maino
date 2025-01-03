6a4cb0a520d1627e3ea504bbf0aef6bdeaf8275e
felipeabdo <felipe.abdo.ti@.com>
2025-01-03 18:33:03 -0300

    feat: basic image upload system working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :author, :body)
+      params.require(:post).permit(:title, :author, :body, :image)
     end
 end
