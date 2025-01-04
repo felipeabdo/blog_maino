38099755dc8cb006697f2d55288da639e9d9b84e
felipeabdo <felipe.abdo.ti@.com>
2025-01-04 11:00:33 -0300

    first post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,9 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  post = Post.new
+  test "Instância de post vazia não será salva" do
+    assert_not post.save, "Post vazio foi salvo"
+  end
 end
